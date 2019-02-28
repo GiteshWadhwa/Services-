020eae6044d3c5053139543f14d38926d53218fe
Gitesh <wadhwagitesh2@gmail.com>
2019-02-28 13:16:30 +0530

    Services in App

---
+++ ServicesDemo/app/src/main/java/com/gitesh/wadhwa/servicesdemo/MyOwnService.java
@@ -0,0 +1,36 @@
+package com.gitesh.wadhwa.servicesdemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyOwnService extends Service {
+    public MyOwnService() {
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Toast.makeText(this,"Service Created",Toast.LENGTH_SHORT).show();
+        Log.i("MyService","Service has Created");
+    }
+    @Override
+    public int onStartCommand(Intent intent,int flags,int startId)
+    {
+        Toast.makeText(this,"Service Started",Toast.LENGTH_SHORT).show();
+        Log.i("MyService","Service has started");
+        return START_STICKY;
+    }
+@Override
+  public void onDestroy(){
+        super.onDestroy();
+    Toast.makeText(this,"Service Stopped",Toast.LENGTH_SHORT).show();
+    Log.i("MyService","Service has stopped");
+}
+    @Override
+    public IBinder onBind(Intent intent) {
+       return null;
+    }
+}
